9460fad0e962f7fbd3f69972c52f32ba2ebc41e4
Indragie Karunaratne <i@indragie.com>
2014-04-27 19:22:40 -0600

    Forgot the header

--- INDSequentialTextSelectionManager.h
+++ INDSequentialTextSelectionManager.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NSAttributedString * (^INDAttributedTextTransformationBlock)(NSAttributedString *);
+
 /**
  *  Coordinates sequential text selection among an arbitrary set of `NSTextView`s
  */
@@ -17,10 +19,11 @@
  *  Registers a text view to participate in sequential selection.
  *
  *  @param textView   The `NSTextView` instance to register.
- *  @param identifier The unique identifier to associate with the text view instance,
- *  used for restoring text view state.
+ *  @param identifier The unique identifier to associate with the text view instance.
+ *  @param block      A transformation block to apply to the contents of the text view
+ *  before copying the text.
  */
-- (void)registerTextView:(NSTextView *)textView withUniqueIdentifier:(NSString *)identifier;
+- (void)registerTextView:(NSTextView *)textView withUniqueIdentifier:(NSString *)identifier transformationBlock:(INDAttributedTextTransformationBlock)block;
 
 /**
  *  Unregisters a text view for sequential text selection.
